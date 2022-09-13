1f2d60802878bcebfe4ced8756742fe3569e9192
Emmanuel <maguey_diseno@outlook.com>
2022-09-13 08:40:16 -0500

    Desarrollo de página para visualizar las quinielas registradas

---
+++ php/ver_registros.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['usuario'])){
+    echo
+    '
+    <script>
+        alert ("Esta sección es solo para usuarios registrados");
+        window.location = "../index.php";
+    </script>
+    ';
+    session_destroy();
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ver quinielas registradas</title>
+</head>
+<body>
+    
+<p>Aqui se verán las tablas de las quinielas registradas</p>
+
+</body>
+</html>
